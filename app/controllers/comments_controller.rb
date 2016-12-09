b51c9716a9ea563e3db4a52299fc8d3f57848912
shu <hataluckytai@gmail.com>
2016-12-09 16:10:20 +0900

    Initial Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+
+# コメント書き込み用
+  def create
+    @board = Board.find(params[:board_id])
+    @comment = Comment.new(params_comment)
+    @comment.board = @board
+
+    if @comment.save
+      redirect_to board_url(@board)
+    else
+      render "board/show"
+    end
+  end
+
+
+# コメント削除用
+  def destroy
+    @board = Board.find(params[:board_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to board_url(@board)
+  end
+
+
+private
+
+def params_comment
+  params.require(:comment).permit(:name, :content)
+end
+
+
+
+
+end
---
+++ app/models/board.rb
@@ -0,0 +1,5 @@
+class Board < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: {in:1..127}
+  validates :editor, presence: true, length: {in:1..30}
+end
